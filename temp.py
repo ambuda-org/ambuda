681721fbb5e15450ec8de9c0a488ed0c367f579a
Arun Prasad <akprasad.public@gmail.com>
2022-08-01 20:52:39 -0700

    Add minimal setup

---
+++ temp.py
@@ -0,0 +1,22 @@
+from ambuda import create_app
+from ambuda.mail import mailer
+from flask_mail import Message
+
+print("0")
+app = create_app("development")
+print("1")
+mailer.init_app(app)
+
+with app.app_context():
+    print("2")
+    for i in range(10):
+        msg = Message(
+            "Twilio SendGrid Test Email", recipients=["recipient@example.com"]
+        )
+        print("3")
+    msg.body = "This is a test email!"
+    msg.html = "<p>This is a test email!</p>"
+
+    print("4")
+    # mailer.send(msg)
+    print("5")
