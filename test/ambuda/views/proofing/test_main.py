dbc6750e5168bc5708116d47d2b3f411e1c46d03
Arun Prasad <akprasad.public@gmail.com>
2022-08-07 20:26:57 -0700

    Fix unit tests

--- test/ambuda/views/proofing/test_main.py
+++ test/ambuda/views/proofing/test_main.py
@@ -1,6 +1,7 @@
 def test_index(client):
     resp = client.get("/proofing/")
     assert resp.status_code == 200
+    assert ">Proofing<" in resp.text
 
 
 def test_beginners_guide(client):
@@ -18,11 +19,11 @@ def test_recent_changes(client):
     assert ">Recent changes<" in resp.text
 
 
-def test_upload_images__unauth(client):
-    resp = client.get("/proofing/create-new-project")
+def test_create_project__unauth(client):
+    resp = client.get("/proofing/create-project")
     assert resp.status_code == 302
 
 
-def test_upload_images__auth(rama_client):
-    resp = rama_client.get("/proofing/create-new-project")
+def test_create_project__auth(rama_client):
+    resp = rama_client.get("/proofing/create-project")
     assert resp.status_code == 200
