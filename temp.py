b6148e0fcd9a5603ba711aa33ea524aafba7d9d1
Arun Prasad <akprasad.public@gmail.com>
2022-08-08 18:22:49 -0700

    WIP -- mail works

--- temp.py
+++ temp.py
@@ -7,16 +7,15 @@ app = create_app("development")
 print("1")
 mailer.init_app(app)
 
+print("1.5")
 with app.app_context():
     print("2")
     for i in range(10):
-        msg = Message(
-            "Twilio SendGrid Test Email", recipients=["recipient@example.com"]
-        )
+        msg = Message("Twilio SendGrid Test Email", recipients=["arun90@gmail.com"])
         print("3")
     msg.body = "This is a test email!"
     msg.html = "<p>This is a test email!</p>"
 
     print("4")
-    # mailer.send(msg)
+    mailer.send(msg)
     print("5")
