84e880cb18113e3570b11ce06fa87e719a3f0b3b
Arun Prasad <akprasad.public@gmail.com>
2022-08-16 19:14:59 -0700

    Migrate proofing interface to Alpine
    In addition:
    - add `Layout` option to support top-and-bottom view.
    - add `Mark text` option to mark up text.

--- ambuda/static/js/proofreading.js
+++ ambuda/static/js/proofreading.js
@@ -1,42 +1,92 @@
-/* global $, Server, OpenSeadragon, IMAGE_URL */
+/* global Alpine, $, OpenSeadragon, IMAGE_URL */
 
-// OCR trigger
-$('#run-ocr').addEventListener('click', (e) => {
-  e.preventDefault();
+/* Transcription and proofreading interface. */
+const CONFIG_KEY = 'proofing-editor';
+const LAYOUT_SIDE_BY_SIDE = 'flex flex-col-reverse md:flex-row h-[90vh]';
+const LAYOUT_TOP_AND_BOTTOM = 'flex flex-col-reverse h-[90vh]';
 
-  const $button = e.target;
-  $button.textContent = '...';
-  $button.disabled = true;
+const editorComponent = () => ({
+  isRunningOCR: false,
+  textRatio: 1,
+  layout: 'side-by-side',
 
-  const { pathname } = window.location;
-  const url = pathname.replace('/proofing/', '/api/ocr/');
+  init() {
+    this.loadConfig();
+  },
+  loadConfig() {
+    const configStr = localStorage.getItem(CONFIG_KEY);
+    if (configStr) {
+      try {
+        const config = JSON.parse(configStr);
+        this.textRatio = config.textRatio;
+        this.layout = config.layout;
+      } catch (error) {
+        // console.error(error);
+      }
+    }
+  },
+  saveConfig() {
+    const config = {
+      textRatio: this.textRatio,
+      layout: this.layout,
+    };
+    localStorage.setItem(CONFIG_KEY, JSON.stringify(config));
+  },
+  async runOCR() {
+    this.isRunningOCR = true;
 
-  Server.getText(url, (text) => {
-    $('[name=content]').value = text;
-    $button.textContent = 'OCR';
-    $button.disabled = false;
-  }, () => {
-    $button.textContent = 'OCR';
-    $button.disabled = false;
-  });
-});
+    const { pathname } = window.location;
+    const url = pathname.replace('/proofing/', '/api/ocr/');
 
-// Resize textarea text size
-$('#text-increase').addEventListener('click', (e) => {
-  e.preventDefault();
-  const $textarea = $('textarea');
-  const oldSize = $textarea.dataset.size || '1.0';
-  const newSize = parseFloat(oldSize) + 0.2;
-  $textarea.dataset.size = newSize;
-  $textarea.style.fontSize = `${newSize}rem`;
-});
-$('#text-decrease').addEventListener('click', (e) => {
-  e.preventDefault();
-  const $textarea = $('textarea');
-  const oldSize = $textarea.dataset.size || '1.0';
-  const newSize = parseFloat(oldSize) - 0.2;
-  $textarea.dataset.size = newSize;
-  $textarea.style.fontSize = `${newSize}rem`;
+    const content = await fetch(url)
+      .then((response) => {
+        if (response.ok) {
+          return response.text();
+        }
+        return '(server error)';
+      });
+    $('[name=content]').value = content;
+
+    this.isRunningOCR = false;
+  },
+  increaseTextSize() {
+    this.textRatio += 0.2;
+    this.saveConfig();
+  },
+  decreaseTextSize() {
+    this.textRatio = Math.max(0, this.textRatio - 0.2);
+    this.saveConfig();
+  },
+  displaySideBySide() {
+    this.layout = LAYOUT_SIDE_BY_SIDE;
+    this.saveConfig();
+  },
+  displayTopAndBottom() {
+    this.layout = LAYOUT_TOP_AND_BOTTOM;
+    this.saveConfig();
+  },
+
+  markAs(before, after) {
+    // FIXME: more idiomatic way to get this?
+    const $textarea = $('#content');
+    const start = $textarea.selectionStart;
+    const end = $textarea.selectionEnd;
+    const { value } = $textarea;
+    const selectedText = value.substr(start, end - start);
+    $textarea.value = value.substr(0, start) + before + selectedText + after + value.substr(end);
+  },
+  markAsError() {
+    this.markAs('<error>', '</error>');
+  },
+  markAsFix() {
+    this.markAs('<fix>', '</fix>');
+  },
+  markAsUnclear() {
+    this.markAs('<flag>', '</flag>');
+  },
+  markAsFootnoteNumber() {
+    this.markAs('[^', ']');
+  },
 });
 
 // Warn the user if leaving the page after changing the text box.
@@ -79,6 +129,10 @@ function initializeImageViewer(imageURL) {
     maxZoomPixelRatio: 2,
   });
 }
+
+window.addEventListener('alpine:init', () => {
+  Alpine.data('editor', editorComponent);
+});
 
 // Setup
 initializeImageViewer(IMAGE_URL);
