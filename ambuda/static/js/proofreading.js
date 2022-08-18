9e89bcb293e75938bc35f0877bb26207d213b953
Arun Prasad <akprasad.public@gmail.com>
2022-08-17 17:48:12 -0700

    Remember zoom preferences in proofing UI
    This doesn't interact very well with manual zooming, but for now this is
    still an improvement.

--- ambuda/static/js/proofreading.js
+++ ambuda/static/js/proofreading.js
@@ -1,37 +1,87 @@
 /* global Alpine, $, OpenSeadragon, IMAGE_URL */
-
 /* Transcription and proofreading interface. */
+
 const CONFIG_KEY = 'proofing-editor';
 const LAYOUT_SIDE_BY_SIDE = 'flex flex-col-reverse md:flex-row h-[90vh]';
 const LAYOUT_TOP_AND_BOTTOM = 'flex flex-col-reverse h-[90vh]';
 
-const editorComponent = () => ({
+/* Initialize our image viewer. */
+function initializeImageViewer(imageURL) {
+  return OpenSeadragon({
+    id: 'osd-image',
+    tileSources: {
+      type: 'image',
+      url: imageURL,
+      buildPyramid: false,
+    },
+
+    // Buttons
+    showZoomControl: false,
+    showHomeControl: false,
+    showRotationControl: true,
+    showFullPageControl: false,
+    // Zoom buttons are defined in the `Editor` component below.
+    rotateLeftButton: 'osd-rotate-left',
+    rotateRightButton: 'osd-rotate-right',
+
+    // Animations
+    gestureSettingsMouse: {
+      flickEnabled: true,
+    },
+    animationTime: 0.5,
+
+    // The zoom multiplier to use when using the zoom in/out buttons.
+    zoomPerClick: 1.1,
+    // Max zoom level
+    maxZoomPixelRatio: 2.5,
+  });
+}
+
+const imageViewer = initializeImageViewer(IMAGE_URL);
+
+const Editor = () => ({
   isRunningOCR: false,
-  textRatio: 1,
+  textZoom: 1,
+  imageZoom: null,
   layout: 'side-by-side',
 
   init() {
-    this.loadConfig();
+    this.loadSettings();
+    // Set `imageZoom` only after the viewer is fully initialized.
+    imageViewer.addHandler('open', () => {
+      this.imageZoom = this.imageZoom || imageViewer.viewport.getHomeZoom();
+      imageViewer.viewport.zoomTo(this.imageZoom);
+    });
   },
-  loadConfig() {
-    const configStr = localStorage.getItem(CONFIG_KEY);
-    if (configStr) {
+
+  // Settings IO
+
+  loadSettings() {
+    const settingsStr = localStorage.getItem(CONFIG_KEY);
+    if (settingsStr) {
       try {
-        const config = JSON.parse(configStr);
-        this.textRatio = config.textRatio;
-        this.layout = config.layout;
+        const settings = JSON.parse(settingsStr);
+        this.textZoom = settings.textZoom || this.textZoom;
+        // We can only get an accurate default zoom after the viewer is fully
+        // initialized. See `init` for details.
+        this.imageZoom = settings.imageZoom;
+        this.layout = settings.layout || this.layout;
       } catch (error) {
-        // console.error(error);
+        console.error(error);
       }
     }
   },
-  saveConfig() {
-    const config = {
-      textRatio: this.textRatio,
+  saveSettings() {
+    const settings = {
+      textZoom: this.textZoom,
+      imageZoom: this.imageZoom,
       layout: this.layout,
     };
-    localStorage.setItem(CONFIG_KEY, JSON.stringify(config));
+    localStorage.setItem(CONFIG_KEY, JSON.stringify(settings));
   },
+
+  // OCR controls
+
   async runOCR() {
     this.isRunningOCR = true;
 
@@ -49,22 +99,48 @@ const editorComponent = () => ({
 
     this.isRunningOCR = false;
   },
+
+  // Image zoom controls
+
+  increaseImageZoom() {
+    this.imageZoom *= 1.2;
+    imageViewer.viewport.zoomTo(this.imageZoom);
+    this.saveSettings();
+  },
+  decreaseImageZoom() {
+    this.imageZoom *= 0.8;
+    imageViewer.viewport.zoomTo(this.imageZoom);
+    this.saveSettings();
+  },
+  resetImageZoom() {
+    this.imageZoom = imageViewer.viewport.getHomeZoom();
+    imageViewer.viewport.zoomTo(this.imageZoom);
+    this.saveSettings();
+  },
+
+  // Text zoom controls
+
   increaseTextSize() {
-    this.textRatio += 0.2;
-    this.saveConfig();
+    this.textZoom += 0.2;
+    this.saveSettings();
   },
   decreaseTextSize() {
-    this.textRatio = Math.max(0, this.textRatio - 0.2);
-    this.saveConfig();
+    this.textZoom = Math.max(0, this.textZoom - 0.2);
+    this.saveSettings();
   },
+
+  // Layout controls
+
   displaySideBySide() {
     this.layout = LAYOUT_SIDE_BY_SIDE;
-    this.saveConfig();
+    this.saveSettings();
   },
   displayTopAndBottom() {
     this.layout = LAYOUT_TOP_AND_BOTTOM;
-    this.saveConfig();
+    this.saveSettings();
   },
+
+  // Markup controls
 
   markAs(before, after) {
     // FIXME: more idiomatic way to get this?
@@ -89,6 +165,11 @@ const editorComponent = () => ({
   },
 });
 
+// Setup
+window.addEventListener('alpine:init', () => {
+  Alpine.data('editor', Editor);
+});
+
 // Warn the user if leaving the page after changing the text box.
 let hasUnsavedChanges = false;
 $('[name=content]').addEventListener('change', () => {
@@ -98,44 +179,6 @@ $('[type=submit]').addEventListener('click', () => {
   hasUnsavedChanges = false;
 });
 
-/* Initialize our image viewer. */
-function initializeImageViewer(imageURL) {
-  OpenSeadragon({
-    id: 'osd-image',
-    tileSources: {
-      type: 'image',
-      url: imageURL,
-      buildPyramid: false,
-    },
-
-    // Buttons
-    showRotationControl: true,
-    showFullPageControl: false,
-    zoomInButton: 'osd-zoom-in',
-    zoomOutButton: 'osd-zoom-out',
-    homeButton: 'osd-home',
-    rotateLeftButton: 'osd-rotate-left',
-    rotateRightButton: 'osd-rotate-right',
-
-    // Animations
-    gestureSettingsMouse: {
-      flickEnabled: true,
-    },
-    animationTime: 0,
-
-    // The zoom multiplier to use when using the zoom in/out buttons.
-    zoomPerClick: 1.2,
-    // Max zoom level
-    maxZoomPixelRatio: 2,
-  });
-}
-
-window.addEventListener('alpine:init', () => {
-  Alpine.data('editor', editorComponent);
-});
-
-// Setup
-initializeImageViewer(IMAGE_URL);
 window.onbeforeunload = () => {
   if (hasUnsavedChanges) {
     return 'You have unsaved changes! If you leave this page, your changes will be lost.';
