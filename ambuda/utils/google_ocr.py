a90f24f9888e05fc34a9db7eba05f120699f7f89
Ashwin Ramaswami <aramaswamis@gmail.com>
2022-08-21 14:19:13 -0400

    Update google_ocr.py

--- ambuda/utils/google_ocr.py
+++ ambuda/utils/google_ocr.py
@@ -21,7 +21,7 @@ def post_process(text: str) -> str:
         # Danda and double danda
         .replace("||", "॥")
         .replace("|", "।")
-        .replace("।।", "।।")
+        .replace("।।", "॥")
         # Remove curly quotes
         .replace("‘", "'")
         .replace("’", "'")
