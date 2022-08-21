35c0ac750800d739c8ac857438f2ba670c9fd709
Ashwin Ramaswami <aramaswamis@gmail.com>
2022-08-21 14:19:47 -0400

    Update test_google_ocr.py

--- test/ambuda/utils/test_google_ocr.py
+++ test/ambuda/utils/test_google_ocr.py
@@ -6,11 +6,13 @@ from ambuda.utils.google_ocr import post_process
 def test_post_process():
     text = """हरिः |
 हरिः ||
+हरिः ।।
 “Hello world”
 ‘Hello world’"""
     assert (
         post_process(text)
         == """हरिः ।
+हरिः ॥
 हरिः ॥
 "Hello world"
 'Hello world'"""
