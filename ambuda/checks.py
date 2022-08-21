e8aad6f19dc36967ec12c295a7efd97e8b7e4fa5
Arun Prasad <akprasad.public@gmail.com>
2022-08-21 09:14:29 -0700

    Add nullable & primary-key checks + docstring details

--- ambuda/checks.py
+++ ambuda/checks.py
@@ -2,9 +2,11 @@
 
 
 import sys
+from typing import Optional
 
 from click import style
 from sqlalchemy import create_engine
+from sqlalchemy.schema import Column
 from sqlalchemy import inspect
 
 from ambuda.models.base import Base
@@ -15,13 +17,47 @@ def _warn(text: str = ""):
     print(style(text, fg="red"))
 
 
+def _full_column_name(table_name: str, col: str) -> str:
+    return f"{table_name}.{col}"
+
+
+def _check_column(app_col: Column, db_col: dict[str, str]) -> list[str]:
+    full_name = _full_column_name(app_col.table.name, app_col.name)
+    errors = []
+
+    if app_col.nullable and not db_col["nullable"]:
+        errors.append(f'Column "{full_name}" is nullable in the app but not in the db.')
+    elif db_col["nullable"] and not app_col.nullable:
+        errors.append(f'Column "{full_name}" is nullable in the db but not in the app.')
+
+    if app_col.primary_key and not db_col["primary_key"]:
+        errors.append(
+            f'Column "{full_name}" is a primary key in the app but not in the db.'
+        )
+    elif db_col["primary_key"] and not app_col.primary_key:
+        errors.append(
+            f'Column "{full_name}" is a primary key in the db but not in the app.'
+        )
+
+    return errors
+
+
 def check_app_schema_matches_db_schema(database_uri: str):
     """Check that our application tables and database tables match.
 
     Currently, we apply the following checks:
+
     - All app tables exist in the database.
     - A given app table and its corresponding database table have the same
       column names.
+    - A given app columns and its database column have equal `nullable` and
+      `primary_key` status.
+
+    Here are some important checks that we haven't implemented yet:
+
+    - Column have equal types.
+    - Column have equal default values.
+    - Column have equal indices.
 
     If any check fails, exit gracefully and tell the user how to resolve the
     issue.
@@ -35,24 +71,34 @@ def check_app_schema_matches_db_schema(database_uri: str):
     errors = []
 
     for table_name, table in Base.metadata.tables.items():
-        app_column_names = set(table.columns.keys())
-        db_column_names = {c["name"] for c in inspector.get_columns(table_name)}
+        app_columns = table.columns
+        db_columns = {c["name"]: c for c in inspector.get_columns(table_name)}
+
+        app_column_names = set(app_columns.keys())
+        db_column_names = set(db_columns.keys())
 
         if not db_column_names:
-            errors.append(f'Table "{table_name}" found in app, missing in db')
+            errors.append(f'Table "{table_name}" found in app but missing in db.')
             continue
 
         for col in app_column_names:
             if col not in db_column_names:
-                errors.append(
-                    f'Column "{table_name}.{col}" found in app, missing in db'
-                )
+                full_name = _full_column_name(table_name, col)
+                errors.append(f'Column "{full_name}" found in app but missing in db.')
 
         for col in db_column_names:
             if col not in app_column_names:
-                errors.append(
-                    f'Column "{table_name}.{col}" found in db, missing in app'
-                )
+                full_name = _full_column_name(table_name, col)
+                errors.append(f'Column "{full_name}" found in db but missing in app.')
+
+        for col in app_column_names & db_column_names:
+            app_col = app_columns[col]
+            db_col = db_columns[col]
+            full_name = _full_column_name(table_name, col)
+
+            column_errors = _check_column(app_col, db_col)
+            if column_errors:
+                errors.extend(column_errors)
 
     if errors:
         _warn("The data tables defined in your application code don't match the")
@@ -69,6 +115,7 @@ def check_app_schema_matches_db_schema(database_uri: str):
         _warn("Ambuda Discord server (https://discord.gg/7rGdTyWY7Z).")
         _warn()
         _warn("Errors found:")
+        _warn()
         for error in errors:
             _warn(f"- {error}")
         sys.exit(1)
