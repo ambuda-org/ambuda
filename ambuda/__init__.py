86233b4af0ca900dbcb416e316e273d64a3de75c
Ashwin Ramaswami <aramaswamis@gmail.com>
2022-08-22 21:46:51 -0400

    Set up CSP, unpkg -> jsdelivr
    As I was writing up a privacy policy in #169, I noticed a few things:
    
    - unpkg doesn't have a privacy policy (see
      https://twitter.com/unpkg/status/1215718511537876992)
    - we probably want to avoid loading as many third-party scripts as
      possible, for privacy reasons
    
    So I switched from unpkg -> jsdelivr for the alpine script, and I also set up a
    CSP header that whitelists domains whose scripts can be loaded on our site.
    This also has the added bonus that inline javascript is disallowed by CSP,
    helping mitigate the risks of XSS attacks.
    
    We'd probably eventually want to cut down on the number of external scripts and
    just get to `script-src 'self'` if possible, though.

--- ambuda/__init__.py
+++ ambuda/__init__.py
@@ -124,4 +124,11 @@ def create_app(config_env: str):
         }
     )
 
+    @app.after_request
+    def add_security_headers(resp):
+        resp.headers[
+            "Content-Security-Policy"
+        ] = "default-src 'self'; script-src 'self' https://cdn.jsdelivr.net https://www.google.com https://www.gstatic.com https://plausible.io; frame-src https://www.google.com; img-src 'self' data:;"
+        return resp
+
     return app
