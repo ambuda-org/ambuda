e258fa02aea38b3def438de4c153b20ad4daad41
Arun Prasad <akprasad.public@gmail.com>
2022-08-23 19:40:53 -0700

    Rename various files
    - Rename `projects.py` to `project.py` since its routes correspond to a
      single project.
    - Rename `pages.py` to `page.py` since its routes correspond to a
      single page.
    - Rename `cheda.py` to `parses.py` for clarity.
    - Rename the corresponding test files for the blueprints above.
    - Delete `test.py`, which is unused debugging code from an earlier PR.

--- ambuda/views/proofing/__init__.py
+++ ambuda/views/proofing/__init__.py
@@ -1,12 +1,12 @@
-from . import main, pages, projects, tagging, talk, users
+from . import main, page, project, tagging, talk, users
 
 
 __all__ = ["bp"]
 
 
 bp = main.bp
-bp.register_blueprint(projects.bp)
-bp.register_blueprint(pages.bp)
+bp.register_blueprint(project.bp)
+bp.register_blueprint(page.bp)
 bp.register_blueprint(tagging.bp, url_prefix="/tagging")
 bp.register_blueprint(talk.bp)
 bp.register_blueprint(users.bp, url_prefix="/users")
