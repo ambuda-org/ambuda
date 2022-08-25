4834934d60841718951d1d7e6a9b277bedf72ebe
Ashwin Ramaswami <aramaswamis@gmail.com>
2022-08-25 20:18:12 +0000

    update

--- ambuda/__init__.py
+++ ambuda/__init__.py
@@ -145,11 +145,4 @@ def create_app(config_env: str):
         }
     )
 
-    # @app.after_request
-    # def add_security_headers(resp):
-    #     resp.headers[
-    #         "Content-Security-Policy"
-    #     ] = "default-src 'self'; script-src 'self' https://cdn.jsdelivr.net https://www.google.com https://www.gstatic.com https://plausible.io; frame-src https://www.google.com; img-src 'self' data:; style-src 'self';"
-    #     return resp
-
     return app
