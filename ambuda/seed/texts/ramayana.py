95d66839054d32388a99c72e070870a8562c5b67
Ashwin Ramaswami <aramaswamis@gmail.com>
2022-08-25 23:25:48 -0400

    Add headers for ramayana and mahabharata texts
    Previously the "about" pages for these texts were empty. This adds TEI headers so the "about" page has more useful info:

--- ambuda/seed/texts/ramayana.py
+++ ambuda/seed/texts/ramayana.py
@@ -7,6 +7,24 @@ import ambuda.seed.utils.itihasa_utils as iti
 
 BASE_URL = "https://bombay.indology.info/ramayana/text/UD/Ram{n}.txt"
 
+TEI_HEADER = """<teiHeader xml:lang="en">
+  <fileDesc>
+    <titleStmt>
+        <title>Rāmāyaṇa</title>
+        <author>Vālmīki</author>
+    </titleStmt>
+
+    <publicationStmt>
+        <publisher>bombay.indology.info</publisher>
+        <availability>
+        <p>The electronic text of the Rāmāyaṇa is sourced from <ref target="https://bombay.indology.info/ramayana/statement.html">John Smith's website</ref> and was originally published by the Oriental Institute, Baroda.</p>
+
+        </availability>
+    </publicationStmt>
+  </fileDesc>
+</teiHeader>
+"""
+
 
 def iter_lines(raw: str):
     for line in raw.splitlines():
@@ -61,7 +79,12 @@ def run():
 
     print("Writing text ...")
     iti.write_kandas(
-        engine, kandas, text_slug=text_slug, text_title="rAmAyaNam", xml_id_prefix="R"
+        engine,
+        kandas,
+        text_slug=text_slug,
+        text_title="rAmAyaNam",
+        tei_header=TEI_HEADER,
+        xml_id_prefix="R",
     )
 
     print("Done.")
--- ambuda/xml.py
+++ ambuda/xml.py
@@ -45,9 +45,9 @@ class Rule:
     #: desired format.
     attrib_fn: Callable
     #: Text to insert before this element's `text` field.
-    text_before: str
+    text_before: str = ""
     #: Text to insert after this element's `tail` field.
-    text_after: str
+    text_after: str = ""
 
     def __call__(self, el: ET.Element):
         el.tag = self.tag
@@ -242,6 +242,7 @@ tei_header_xml = {
     "publisher": None,
     "bibl": elem("p"),
     "licence": elem("p"),
+    "ref": Rule("a", _rename({"target": "href"})),
 }
 
 
