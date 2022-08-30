2e149c5c91d1f85f4fd9b4644ccdaec273607970
Arun Prasad <akprasad.public@gmail.com>
2022-08-29 22:04:41 -0700

    Fix proofing glob path in Tailwind config
    Otherwise, we miss some classes.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,6 @@ module.exports = {
     './ambuda/templates/**/*.html',
     './ambuda/utils/parse_alignment.py',
     './ambuda/utils/xml.py',
-    './ambuda/views/proofing.py',
+    './ambuda/views/proofing/main.py',
   ],
 }
