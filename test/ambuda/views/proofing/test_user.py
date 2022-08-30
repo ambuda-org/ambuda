350d812efe1eae0253be850ca866125c6677163f
Arun Prasad <akprasad.public@gmail.com>
2022-08-30 08:56:07 -0700

    Add more tests

--- test/ambuda/views/proofing/test_user.py
+++ test/ambuda/views/proofing/test_user.py
@@ -3,6 +3,36 @@ def test_summary(client):
     assert resp.status_code == 200
 
 
+def test_edit__user_match(rama_client):
+    resp = rama_client.get("/proofing/users/ramacandra/edit")
+    assert resp.status_code == 200
+
+
+def test_edit__user_match__post(rama_client):
+    resp = rama_client.get("/proofing/users/ramacandra/")
+    assert resp.status_code == 200
+    assert "My description" not in resp.text
+
+    resp = rama_client.post(
+        "/proofing/users/ramacandra/edit", data={"description": "My description"}
+    )
+    assert resp.status_code == 302
+
+    resp = rama_client.get("/proofing/users/ramacandra/")
+    assert resp.status_code == 200
+    assert "My description" in resp.text
+
+
+def test_edit__user_mismatch(rama_client):
+    resp = rama_client.get("/proofing/users/akprasad/edit")
+    assert resp.status_code == 403
+
+
+def test_edit__unauth(client):
+    resp = client.get("/proofing/users/ramacandra/edit")
+    assert resp.status_code == 302
+
+
 def test_summary__missing(client):
     resp = client.get("/proofing/users/bad-user/")
     assert resp.status_code == 404
