43d80aff8cf845b435b3fe8544231dd47d16303e
Arun Prasad <akprasad.public@gmail.com>
2022-09-03 12:11:44 -0700

    Update mixin check

--- test/ambuda/utils/test_user_mixins.py
+++ test/ambuda/utils/test_user_mixins.py
@@ -1,4 +1,6 @@
 from ambuda import database as db
+from ambuda import queries as q
+from ambuda.enums import SiteRole
 from ambuda.utils.user_mixins import AmbudaAnonymousUser, AmbudaUserMixin
 
 
@@ -11,10 +13,14 @@ def test_anonymous_user():
     assert not u.is_admin
 
 
-def test_new_authenticated_user():
+def test_new_authenticated_user(client):
     u = db.User()
-    assert not u.is_p1
+    session = q.get_session()
+    p1 = session.query(db.Role).filter_by(name=SiteRole.P1).one()
+
+    u.roles = [p1]
+    assert u.is_p1
     assert not u.is_p2
-    assert not u.is_proofreader
+    assert u.is_proofreader
     assert not u.is_moderator
     assert not u.is_admin
