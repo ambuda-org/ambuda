76eeab51a3bb789919945de34218ea0a4cf5100e
Kishore Chitrapu <chitrapu@gmail.com>
2022-09-04 20:10:03 -0400

    fixed database.py and itihasa utils

--- ambuda/database.py
+++ ambuda/database.py
@@ -5,7 +5,7 @@
 from ambuda.models.auth import *  # NOQA F401,F403
 from ambuda.models.base import Base  # NOQA F401,F403
 from ambuda.models.dictionaries import *  # NOQA F401,F403
-from ambuda.models.parse import * # NOQA F401,F403
-from ambuda.models.proofing import * # NOQA F401,F403
+from ambuda.models.parse import *  # NOQA F401,F403
+from ambuda.models.proofing import *  # NOQA F401,F403
 from ambuda.models.talk import *  # NOQA F401,F403
 from ambuda.models.texts import *  # NOQA F401,F403
