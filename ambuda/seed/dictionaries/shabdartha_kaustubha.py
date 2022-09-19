3fe8fb0902352a0ffdb33cc6b0c39148c0ce677b
Arun Prasad <akprasad.public@gmail.com>
2022-09-18 21:44:26 -0700

    Add option to read latest data in seed scripts
    I added this option to just the `shabdartha_kaustubha` since it has new
    upstream data we want to fetch. We can lazily add this option to our
    other seed scripts as needed, but they tend to be more stable or have a
    Git fetch built-in.
    
    In addition, I moved some functions in `itihasa_utils.py` into a new
    module `data_utils.py`.
    
    Test plan: ran the script.

--- ambuda/seed/dictionaries/shabdartha_kaustubha.py
+++ ambuda/seed/dictionaries/shabdartha_kaustubha.py
@@ -10,7 +10,7 @@ import click
 from indic_transliteration import sanscript
 
 from ambuda.seed.utils.cdsl_utils import create_from_scratch
-from ambuda.seed.utils.data_utils import fetch_text, create_db
+from ambuda.seed.utils.data_utils import create_db, fetch_text
 from ambuda.utils.dict_utils import standardize_key
 
 RAW_URL = "https://github.com/indic-dict/stardict-sanskrit/raw/master/sa-head/other-indic-entries/shabdArtha_kaustubha/shabdArtha_kaustubha.babylon"
