7b13f0803b6deeed323b2c9c4117e44d360db8ee
śrīnivāsa kaśyap munukutla <skmnktl@gmail.com>
2022-09-29 21:45:13 -0400

    pdf rendering v0

--- render_pdf/src/make_latex.py
+++ render_pdf/src/make_latex.py
@@ -1,121 +1,92 @@
 from typing import Union
 import parser
 import os
+import jinja2
 
-BASE = r"""
-\documentclass{tufte-handout}
+# TODO probably best to use pydantic for a config template for the dict below. 
+data = {
+        'language' : "sanskrit",
+        'title' : "",
+        'author' : "",
+        'body' : [],
+        'analysis': []
+    }
 
-\title{::main title::}
+os.chdir("../tex_files")
+latex_jinja_env = jinja2.Environment(
+        block_start_string = '((*',
+        block_end_string = '*))',
+        variable_start_string = '(((',
+        variable_end_string = ')))',
+        comment_start_string = '((=',
+        comment_end_string = '=))',
+        loader = jinja2.FileSystemLoader(os.path.abspath('.'))
+    )
+template = latex_jinja_env.get_template('template.tex')
 
-\author{::author::}
-
-%\date{28 March 2010} % without \date command, current date is supplied
-
-%\geometry{showframe} % display margins for debugging page layout
-
-\usepackage{graphicx} % allow embedded images
-  \setkeys{Gin}{width=\linewidth,totalheight=\textheight,keepaspectratio}
-  \graphicspath{{graphics/}} % set of paths to search for images
-\usepackage{amsmath}  % extended mathematics
-\usepackage{booktabs} % book-quality tables
-\usepackage{units}    % non-stacked fractions and better unit spacing
-\usepackage{multicol} % multiple column layout facilities
-\usepackage{lipsum}   % filler text
-\usepackage{fancyvrb} % extended verbatim environments
-  \fvset{fontsize=\normalsize}% default font size for fancy-verbatim environments
-
-% Standardize command font styles and environments
-\newcommand{\doccmd}[1]{\texttt{\textbackslash#1}}% command name -- adds backslash automatically
-\newcommand{\docopt}[1]{\ensuremath{\langle}\textrm{\textit{#1}}\ensuremath{\rangle}}% optional command argument
-\newcommand{\docarg}[1]{\textrm{\textit{#1}}}% (required) command argument
-\newcommand{\docenv}[1]{\textsf{#1}}% environment name
-\newcommand{\docpkg}[1]{\texttt{#1}}% package name
-\newcommand{\doccls}[1]{\texttt{#1}}% document class name
-\newcommand{\docclsopt}[1]{\texttt{#1}}% document class option name
-\newenvironment{docspec}{\begin{quote}\noindent}{\end{quote}}% command specification environment
-
-\usepackage[parfill]{parskip}
-\usepackage{fontspec}
-\usepackage[english]{babel}
-\usepackage{microtype}
-
-% Sanskrit Support %
-\babelprovide[import]{sanskrit-devanagari}
-\babelprovide[import]{english}
-\babelprovide[import]{greek}
-\babelprovide[import]{german}
-\babelfont[english]{rm}{Bitter}
-\babelfont[sanskrit-devanagari]{rm}
-          {Siddhanta}
-\babelfont[sanskrit-devanagari]{sf}
-          [Language=Default]{Noto Sans}
-
-          
-\newcommand\textsanskrit[1]{\foreignlanguage{sanskrit-devanagari}{#1}}
-\newenvironment{sanskrit}%
-{\begin{otherlanguage}{sanskrit-devanagari}}%
-{\end{otherlanguage}}
-%%%%%%%%%%%%%%%%%%
-
-
-\begin{document}
-\maketitle
-
-::verses::
-
-\end{document}
-"""
-
-
-def set_title(base: str, title:str) -> str:
-    return base.replace(r"::main title::",title)
-
-def set_author(base: str, author: Union[str,None]) -> str:
-    if author:
-        return base.replace(r"::author::", author)
-    else:
-        return base.replace(r"\\author{::author::}", "")
-
-def set_verses(base: str, verses: str) -> str:
-    return base.replace(r"::verses::", verses)
+## TODO add an argument for receiving language, default is sanskrit + devanagari
 
 def write_tex(tex: str):
     os.chdir("../tex_files")
     with open("./template.tex", 'w') as f:
         f.write(tex)
 
-def generate_texfile(link: str, lipi: Union["SKT","IAST"], author: str) -> str:
-    verses, analysis, title = parser.parse(url=link)
-    latex = ""
-    if lipi == "SKT":
-        begin = r"\begin{sanskrit}"
-        end = r"\end{sanskrit}"
-    else:
-        begin = ""
-        end = ""
+def generate_fields(link: str, lipi: Union["SKT","IAST"], author: str) -> tuple[list, str, str]:
+    # TODO handle lipi input
+    _ = lipi
+    #####
 
+    verses, analysis, title, parsed_author = parser.parse(url=link)
+    if parsed_author != "":
+        author = parsed_author
+    parsed_verses = []
+    parsed_analysis = []
+    
     for verse in verses:
-        v = f"\\ {verse}" + r"\\"
         for line in verses[verse]:
+            l = [verse]
             for segment in verses[verse][line]:
                 text = verses[verse][line][segment]
-                if segment in "bd":
-                    v+=text+r"\\"
-                else:
-                    v+=text+" "
-        latex+=f"{begin} {v}\\ {end}"
-    tex = set_title(BASE, title)
-    tex = set_author(tex, author)
-    tex = set_verses(tex,latex)
-    write_tex(tex)
+                l.append(text)
+            parsed_verses.append(l)
+    for verse in analysis:
+        for line in verses[verse]:
+            l = [verse]
+            for segment in verses[verse][line]:
+                text = verses[verse][line][segment]
+                l.append(text)
+            parsed_analysis.append(l)
 
+    return parsed_verses, parsed_analysis, title, author
+
+def generate_texfile(link, lipi, author):
+    # TODO Author can either be passed in or extracted. Some texts don't have this. 
+    # Need to standardize input texts. 
+    body, analysis, title, author = generate_fields(link, lipi, author)
+    print(f"[Title] {title}")
+    print(f"[Author] {author}")
+    global data
+    data['body'] = body
+    data['title'] = title
+    data['author'] = author
+    data['analysis'] = analysis
+    if analysis == []:
+        data['hasAnalysis'] = False
+    else:
+        data['hasAnalysis'] = True
+    rendered = template.render(data)
+    with open("../tex_files/output.tex", 'w') as f:
+        f.write(rendered)
 def generate_pdf(link: str, lipi: Union["SKT","IAST"], author: str):
     generate_texfile(link, lipi, author)
     os.chdir("../tex_files")
     os.system("fish runscript")
 
-
-if __name__ == "__main__":
-    link = "https://raw.githubusercontent.com/ambuda-org/gretil/main/1_sanskr/tei/sa_zivopaniSad.xml"
+def main():
+    link = "https://raw.githubusercontent.com/ambuda-org/gretil/main/1_sanskr/tei/sa_kAlidAsa-kumArasaMbhava.xml"
     generate_pdf(link, "IAST", "apauruṣeya") 
-    
+   
+if __name__ == "__main__":
+    main()
+
+
--- render_pdf/src/parser.py
+++ render_pdf/src/parser.py
@@ -7,8 +7,27 @@ from urllib.request import urlopen
 def get_body(root):
     return root.find(tagname("text")).find(tagname("body"))
 
-def get_title(root):
-    return root.find(tagname("head")).text
+def get_title(root, alt_root):
+    # God this is ugly # TODO
+    try:
+        return root.find(tagname("head")).text
+    except:
+        try:
+            root = alt_root
+            tag = root.find(tagname("teiHeader"))\
+                    .find(tagname("fileDesc")) \
+                    .find(tagname("titleStmt"))
+            return tag.find(tagname("title")).text
+        except:
+            print("[Warning] Couldn't get title.")
+            return ""
+
+def get_author(root):
+    try:
+        return root.find(tagname("titleStmt"))\
+                .find(tagname("author")).text
+    except:
+        return ""
 
 def get_text_div(root):
     return root.find(tagname("div"))
@@ -26,23 +45,35 @@ def parse_lg(lg,verse=None):
 
 def parse_note(note):
     corresp = note.get("corresp").replace("#","")
+
     return parse_lg(note.find(tagname("lg")), corresp)
 
+def parse_div(div):
+    verses = []
+    for lg in div:
+        verses.append(parse_lg(lg))
+    return verses
 # Can be multithreaded.  
 def parse_text(root):
     verses = {}
     analysis = {}
-    for child in root:
+    for child in root.iter():
         if child.tag == tagname("lg"):
             v,txt = parse_lg(child)
             verses[v] = txt
         if child.tag == tagname("note"):
             v,txt = parse_note(child)
             analysis[v] = txt
+        """
+        if child.tag == tagname("div"):
+            vs = parse_div(child)
+            for v in vs:
+                verses[v[0]] = v[1]
+        """
     return verses, analysis
 
 
-# CLUNKY Fix this. Closure maybe? 
+# CLUNKY Fix this. Closure maybe?
 root_url = ""
 
 def tagname(tag):
@@ -69,8 +100,10 @@ def parse_from_string(inpt: str) -> (dict, dict):
 def parse_from_root(root) -> (dict, dict):
     global root_url
     root_url = root.tag.replace("TEI","").replace("{","").replace("}","")
-    verses, analysis = parse_text(get_text_div(get_body(root)))
-    return verses, analysis, get_title(get_body(root))
+    print(f"[PARSING] {get_text_div(get_body(root))}")
+    body = get_body(root)
+    verses, analysis = parse_text(get_text_div(body))
+    return verses, analysis, get_title(body, root), get_author(body)
 
 
 # probably need to use asyncio here? 
