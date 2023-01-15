dd9cd0be0aa38bd53be23fa38f140f1732c94784
Arun Prasad <akprasad.public@gmail.com>
2023-01-14 17:16:24 -0800

    Add basic hook-up

--- ambuda/static/js/cheda.js
+++ ambuda/static/js/cheda.js
@@ -1,10 +1,23 @@
+const CHEDA_URL = "/api/vidyut/cheda"
+
 export default () => ({
     input: null,
+    output: null,
 
     init() {
-        console.log("init");
+        this.input = "बाह्योद्यानस्थितहरशिरश्चन्द्रिकाधौतहर्म्या";
+        this.analyze();
     },
-    analyze() {
+    async analyze() {
         console.log("analyze", this.input);
+
+        const response = await fetch(CHEDA_URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ input: this.input }),
+        });
+        this.output = await response.text();
     },
 });
--- ambuda/views/tools.py
+++ ambuda/views/tools.py
@@ -1,12 +1,29 @@
 """Views for basic site pages."""
 
-from flask import Blueprint, redirect, render_template, session, url_for
+from indic_transliteration import detect, sanscript
+
+from flask import Blueprint, render_template, jsonify, request
+from vidyut import Chedaka
 
 from ambuda import queries as q
+from ambuda.views.api import bp as api
 
 bp = Blueprint("tools", __name__)
+
+c = Chedaka("/Users/akp/projects/vidyut/vidyut-cheda/data/vidyut-0.1.0")
 
 
 @bp.route("/cheda")
 def cheda():
     return render_template("tools/cheda.html")
+
+
+@api.route("/vidyut/cheda", methods=["POST"])
+def api_vidyut_cheda():
+    data = request.json
+    input_ = data.get("input") or ""
+
+    encoding = detect.detect(input_)
+    input_slp1 = sanscript.transliterate(input_, encoding, "slp1")
+    tokens = c.tokenize(input_slp1)
+    return render_template("htmx/cheda.html", tokens=tokens)
