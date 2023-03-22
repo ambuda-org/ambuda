2cffa01f2e4b3fb18130783841b6d3046016d9bf
Kishore Chitrapu <chitrapu@gmail.com>
2023-03-21 22:57:23 -0400

    add unittest for submit_changes

--- test/ambuda/views/proofing/test_project.py
+++ test/ambuda/views/proofing/test_project.py
@@ -134,11 +134,10 @@ def test_replace__bad_project(rama_client):
 def test_submit_changes(moderator_client):
     query = "test_query"
     replace = "test_replace"
-    form_data = {
-        "query": query,
-        "replace": replace
-    }
-    resp = moderator_client.post("/proofing/test-project/submit_changes", data=form_data)
+    form_data = {"query": query, "replace": replace}
+    resp = moderator_client.post(
+        "/proofing/test-project/submit_changes", data=form_data
+    )
     assert "Changes:" in resp.text
 
 
