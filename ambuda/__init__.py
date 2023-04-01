9a84ba5c91b8f2e19faafb6480901080abe7f15a
Kishore Chitrapu <chitrapu@gmail.com>
2023-04-01 00:22:04 -0400

    fixes #483

--- ambuda/__init__.py
+++ ambuda/__init__.py
@@ -111,12 +111,12 @@ def create_app(config_env: str):
     # Database
     _initialize_db_session(app, config_env)
 
-    # Extensions
-    babel = Babel(app)
-
-    @babel.localeselector
     def get_locale():
         return session.get("locale", config_spec.BABEL_DEFAULT_LOCALE)
+
+    # Extensions
+    babel = Babel(app)
+    babel.init_app(app, locale_selector=get_locale)
 
     login_manager = auth_manager.create_login_manager()
     login_manager.init_app(app)
